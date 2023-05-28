3f1092ad5bee389699db0e2f3b503d313a37a209
ZoyaKramarenko <zoyazuberg@gmail.com>
2023-05-28 13:59:38 +0300

    first commit

---
+++ tests/test_calculator.py
@@ -0,0 +1,23 @@
+import pytest
+
+from app.calculator import Calculator
+
+
+class TestCalculator:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_success(self):
+        assert self.calc.multiply(self, 2, 3) == 6
+
+    def test_division_success(self):
+        assert self.calc.division(self, 4, 2) == 2
+
+    def test_adding_success(self):
+        assert self.calc.adding(self, 5, 3) == 8
+
+    def test_subtraction_success(self):
+        assert self.calc.subtraction(self, 8, 3) == 5
+
+    def teardown(self):
+        print('Выполнение метода Teardown')
